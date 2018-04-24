c9cf6962a40e6a782903e2cf9cb3e2b37296bfc0
Olav bøthun <38346531+olav201@users.noreply.github.com>
2018-04-24 14:10:04 +0200

    Add files via upload

---
+++ Scripts/GameController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    public static GameController GameControllerInstance;
+    public Slider healthSlider;
+    public float PlayerHealth;
+
+    private Quaternion originalCameraRotation;
+
+    void Start()
+    {
+        GameControllerInstance = this;
+        originalCameraRotation = Camera.main.transform.rotation;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        healthSlider.value = PlayerHealth;
+    }
+
+    private void ResetCameraRotation()
+    {
+        Camera.main.transform.rotation = originalCameraRotation;
+    }
+
+    public void LoadLevel(int LevelToLoad)
+    {
+        SceneManager.LoadScene(LevelToLoad);
+    }
+}
---
+++ Scripts/PlayerVariables.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playervariables : MonoBehaviour
+{
+
+    public Transform startPosition;
+    public float health = 100;
+
+    private float damageTimer;
+
+
+    void Start()
+    {
+        health = 100;
+    }
+
+
+    void Update()
+    {
+        damageTimer += Time.deltaTime;
+        GameController.GameControllerInstance.PlayerHealth = health;
+    }
+
+    public void Harm(float dmg)
+    {
+        if (damageTimer > 1.0f)
+        {
+            health -= dmg;
+            damageTimer = 0;
+        }
+
+        if (health < 1)
+        {
+            Respawn();
+        }
+    }
+
+    public void Respawn()
+    {
+        transform.position = startPosition.position;
+        health = 100;
+    }
+}
