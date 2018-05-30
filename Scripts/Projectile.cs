99af7c5dccd819a408bd05edc004e81abf719560
Olav bøthun <38346531+olav201@users.noreply.github.com>
2018-05-30 14:58:34 +0200

    Add files via upload
    scriptet fra linken jeg sente, trenger å redigeres med aim controll kansje for å kunne kontrolere tauet som en sving mayby? tenker bare høgt så ta dette med en klype salt.

---
+++ Scripts/Projectile.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using System.Collections.Generic;
+
+public class Projectile : MonoBehaviour
+{
+
+    [Tooltip("Position we want to hit")]
+    public Vector3 targetPos;
+
+    [Tooltip("Horizontal speed, in units/sec")]
+    public float speed = 10;
+
+    [Tooltip("How high the arc should be, in units")]
+    public float arcHeight = 1;
+
+
+    Vector3 startPos;
+
+    void Start()
+    {
+        // Cache our start position, which is really the only thing we need
+        // (in addition to our current position, and the target).
+        startPos = transform.position;
+    }
+
+    void Update()
+    {
+        // Compute the next position, with arc added in
+        float x0 = startPos.x;
+        float x1 = targetPos.x;
+        float dist = x1 - x0;
+        float nextX = Mathf.MoveTowards(transform.position.x, x1, speed * Time.deltaTime);
+        float baseY = Mathf.Lerp(startPos.y, targetPos.y, (nextX - x0) / dist);
+        float arc = arcHeight * (nextX - x0) * (nextX - x1) / (-0.25f * dist * dist);
+        nextPos = new Vector3(nextX, baseY + arc, transform.position.z);
+
+        // Rotate to face the next position, and then move there
+        transform.rotation = LookAt2D(nextPos - transform.position);
+        transform.position = nextPos;
+
+        // Do something when we reach the target
+        if (nextPos == targetPos) Arrived();
+    }
+
+    void Arrived()
+    {
+        Destroy(gameObject);
+    }
+
+    /// 
+    /// This is a 2D version of Quaternion.LookAt; it returns a quaternion
+    /// that makes the local +X axis point in the given forward direction.
+    /// 
+    /// forward direction
+    /// Quaternion that rotates +X to align with forward
+    static Quaternion LookAt2D(Vector2 forward)
+    {
+        return Quaternion.Euler(0, 0, Mathf.Atan2(forward.y, forward.x) * Mathf.Rad2Deg);
+    }
+}
